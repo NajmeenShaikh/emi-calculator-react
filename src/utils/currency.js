6353575eda5de2b222160a224cb5a129df28ea3d
Najmeen Shaikh <55970287+NajmeenShaikh@users.noreply.github.com>
2026-09-08 12:16:20 +0530

    feat: add INR currency formatter

---
+++ src/utils/currency.js
@@ -0,0 +1,9 @@
+const inrFormatter = new Intl.NumberFormat("en-IN", {
+  style: "currency",
+  currency: "INR",
+  maximumFractionDigits: 0,
+});
+
+export function formatINR(value) {
+  return inrFormatter.format(Number(value) || 0);
+}
